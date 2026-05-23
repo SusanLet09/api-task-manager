40e134dde87c7e596c4a1b3b39af64c43878ec30
Susana Leticia <156046612+SuziLet@users.noreply.github.com>
2026-05-22 18:41:48 -0600

    Create task entity

---
+++ src/main/java/com/taskmanager/entity/Task.java
@@ -0,0 +1,34 @@
+package com.taskmanager.entity;
+
+import com.taskmanager.enums.TaskStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "TASK")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    private LocalDateTime creationDate;
+}
